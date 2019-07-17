24378470d5163f69f1ec86ba1404441a7f7e90bc
Jacob Moberg <moberg_17@hotmail.com>
2019-07-17 20:40:19 +0200

    Add OddOrEven.java

---
+++ OddOrEven.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class OddOrEven
+{
+    public static void main(String[] args)
+    {
+        int number = 0;
+        System.out.print("Enter an integer: ");
+        Scanner sc = new Scanner(System.in);
+
+        // Make sure with catch the case where the user doesn't enter a integer
+        try
+        {
+          number = sc.nextInt();
+          // If number % 2 equals 0 it's even otherwhise it's odd
+          final String msg = (number % 2 == 0) ? "Input number is even!"
+          : "Input number is odd!";
+
+          System.out.println(msg);
+        } catch (Exception e)
+        {
+          System.out.println(
+            "Did your really enter a integer?\nIf you did the number might be "
+             + "to big.\nPlease try again...");
+        }
+    }
+}
